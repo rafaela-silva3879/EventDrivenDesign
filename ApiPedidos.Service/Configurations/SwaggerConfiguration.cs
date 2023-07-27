1e755284f345b610edbdc004a9e391b5fed99ff6
rafaela-silva3879 <rafaela.silva3879@outlook.com>
2023-07-27 16:17:12 -0300

    feat: customizar a página de documentação do swagger

---
+++ ApiPedidos.Service/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ApiPedidos.Domain.Entities;
+using Microsoft.OpenApi.Models;
+
+namespace ApiPedidos.Service.Configurations
+{
+    public class SwaggerConfiguration
+    {
+        public static void AddSwagger(WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                new OpenApiInfo
+                {
+                    Title = @"API para controle de pedidos",
+                Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Rafaela Silva",
+                        Email = "rafaela.silva3879@outlook.com",
+                        Url = new
+                Uri("https://github.com/rafaela-silva3879?tab=repositories")
+                    }
+                });
+            });
+        }
+    }
+}
--- ApiPedidos.Service/Program.cs
+++ ApiPedidos.Service/Program.cs
@@ -13,12 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+SwaggerConfiguration.AddSwagger(builder);
 
 // Registre a classe de configuração DependencyInjectionConfiguration
 DependencyInjectionConfiguration.ConfigureServices(builder.Services, builder.Configuration);
